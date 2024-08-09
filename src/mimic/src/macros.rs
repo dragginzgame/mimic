5485760956cd3c54aef89596ae72f10182c9ee3f
borovan <84897664+borovan@users.noreply.github.com>
2024-08-09 12:17:09 +0200

    fixing build macro

--- src/mimic/src/macros.rs
+++ src/mimic/src/macros.rs
@@ -10,38 +10,31 @@ macro_rules! mimic_build {
         use std::io::{self, Write};
         use std::path::PathBuf;
 
-        fn main() -> io::Result<()> {
-            // init
-            mimic_base::init();
+        // cargo directives
+        println!("cargo:rerun-if-changed=build.rs");
 
-            // cargo directives
-            println!("cargo:rerun-if-changed=build.rs");
+        // Get the output directory set by Cargo
+        let out_dir = env::var("OUT_DIR").expect("OUT_DIR not set");
 
-            // Get the output directory set by Cargo
-            let out_dir = env::var("OUT_DIR").expect("OUT_DIR not set");
+        //
+        // actor
+        //
+        let output = ::mimic::build::actor($actor).unwrap();
 
-            //
-            // actor
-            //
-            let output = build::actor($actor).unwrap();
+        // Write the output (stdout) to the specified file in OUT_DIR
+        let actor_file = PathBuf::from(out_dir.clone()).join("actor.rs");
+        let mut file = File::create(actor_file)?;
+        file.write_all(output.as_bytes())?;
 
-            // Write the output (stdout) to the specified file in OUT_DIR
-            let actor_file = PathBuf::from(out_dir.clone()).join("actor.rs");
-            let mut file = File::create(actor_file)?;
-            file.write_all(output.as_bytes())?;
+        //
+        // schema
+        //
+        let output = ::mimic::build::schema().unwrap();
 
-            //
-            // schema
-            //
-            let output = build::schema().unwrap();
-
-            // Write the output (stdout) to the specified file in OUT_DIR
-            let schema_file = PathBuf::from(out_dir).join("schema.rs");
-            let mut file = File::create(schema_file)?;
-            file.write_all(output.as_bytes())?;
-
-            Ok(())
-        }
+        // Write the output (stdout) to the specified file in OUT_DIR
+        let schema_file = PathBuf::from(out_dir).join("schema.rs");
+        let mut file = File::create(schema_file)?;
+        file.write_all(output.as_bytes())?;
     };
 }
 
