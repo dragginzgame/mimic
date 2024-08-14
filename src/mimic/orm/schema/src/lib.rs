d746313c4bfdcbd8436d2a489ef24709a799b7f5
borovan <84897664+borovan@users.noreply.github.com>
2024-08-14 16:33:35 +0200

    get_schema_json

--- src/mimic/orm/schema/src/lib.rs
+++ src/mimic/orm/schema/src/lib.rs
@@ -17,6 +17,9 @@ use snafu::Snafu;
 pub enum Error {
     #[snafu(transparent)]
     Build { source: build::BuildError },
+
+    #[snafu(display("serde json error: {msg}"))]
+    SerdeJson { msg: String },
 }
 
 ///
@@ -30,4 +33,14 @@ pub trait Schemable {
     // generates the structure which is passed to the static Schema data structure
     // via the ctor crate
     fn schema(&self) -> TokenStream;
+}
+
+// get_schema_json
+// to get the built schema via an executable
+pub fn get_schema_json() -> Result<String, Error> {
+    let schema = build::schema_read();
+    let json =
+        serde_json::to_string(&*schema).map_err(|e| Error::SerdeJson { msg: e.to_string() })?;
+
+    Ok(json)
 }
