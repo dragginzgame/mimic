a9d33577dbeb8fc1bede4e9d643b27ba0f11b799
borovan <84897664+borovan@users.noreply.github.com>
2024-09-13 16:49:22 +0200

    added add_wasm to core api

--- src/mimic/api/src/core/wasm.rs
+++ src/mimic/api/src/core/wasm.rs
@@ -12,6 +12,11 @@ pub enum Error {
     CoreWasm { source: core_wasm::Error },
 }
 
+// add_wasm
+pub fn add_wasm(path: &'static str, wasm: &'static [u8]) -> Result<(), Error> {
+    WasmManager::add_wasm(path, wasm).map_err(Error::from)
+}
+
 // get_wasm
 pub fn get_wasm(path: &str) -> Result<&'static [u8], Error> {
     WasmManager::get_wasm(path).map_err(Error::from)
