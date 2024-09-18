a9b49309789e44666b260dd6edbf7d499bdf8fc6
borovan <84897664+borovan@users.noreply.github.com>
2024-09-18 14:10:39 +0200

    trying to fix test canister

--- tests/mytest.rs
+++ tests/mytest.rs
@@ -1,21 +1,28 @@
-use quote::quote;
-
 //
 // just a place to mess around with tests while developing
 //
 
 #[cfg(test)]
 mod tests {
-    use super::*;
 
     #[test]
-    fn test_default_validates() { /*
-                                  use mimic_base::test::player::builder::chunk::Chunk;
+    fn test_default_validates() {
+        use test_schema::validate::{MultipleTenType, Validator};
 
-                                  let e = Chunk::default();
-                                  println!("{e:?}");
+        let e = Validator {
+            multiple_ten: 5.into(),
+        };
+        println!("{e:?}");
 
-                                  let errs = e.validate();
-                                  println!("{errs:?}");*/
+        let errs = orm::validate(&e);
+        println!("{errs:?}");
+
+        //
+
+        let e = MultipleTenType::from(5);
+        println!("{e:?}");
+
+        let errs = orm::validate(&e);
+        println!("{errs:?}");
     }
 }
