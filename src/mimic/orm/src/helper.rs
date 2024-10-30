e4df0f3b085806facfbe10a11b26d35104582a81
borovan <84897664+borovan@users.noreply.github.com>
2024-10-30 18:04:48 +0100

    new() for fixture list

--- src/mimic/orm/src/helper.rs
+++ src/mimic/orm/src/helper.rs
@@ -4,9 +4,15 @@ use crate::traits::EntityDynamic;
 /// FixtureList
 ///
 
+#[derive(Debug, Default)]
 pub struct FixtureList(Vec<Box<dyn EntityDynamic + 'static>>);
 
 impl FixtureList {
+    #[must_use]
+    pub fn new() -> Self {
+        Self(Vec::new())
+    }
+
     pub fn push(&mut self, entity: impl EntityDynamic + 'static) {
         self.0.push(Box::new(entity));
     }
