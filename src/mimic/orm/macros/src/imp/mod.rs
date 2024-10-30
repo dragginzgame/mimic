7aad36c3a2c32ceea794e61b92a6dfe92a19775c
borovan <84897664+borovan@users.noreply.github.com>
2024-10-30 19:24:37 +0100

    auto implement trait

--- src/mimic/orm/macros/src/imp/mod.rs
+++ src/mimic/orm/macros/src/imp/mod.rs
@@ -60,6 +60,7 @@ pub fn any<N: MacroNode>(node: &N, t: Trait) -> TokenStream {
 
         // empty implementations are generated for these traits
         Trait::EntityFixture
+        | Trait::EntityKey
         | Trait::Filterable
         | Trait::Orderable
         | Trait::SanitizeManual
