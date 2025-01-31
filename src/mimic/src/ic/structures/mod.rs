959940771b2f68b6f4e365b18b445c2f29ff1494
borovan <84897664+borovan@users.noreply.github.com>
2025-01-31 16:45:30 +0100

    defaultmemory

--- src/mimic/src/ic/structures/mod.rs
+++ src/mimic/src/ic/structures/mod.rs
@@ -2,6 +2,10 @@ pub mod btreemap;
 pub mod cell;
 pub mod serialize;
 
+// helper
+pub type DefaultMemory =
+    ic_stable_structures::memory_manager::VirtualMemory<ic_stable_structures::DefaultMemoryImpl>;
+
 // re-export
 pub use ic_stable_structures::*;
 pub mod memory {
