b33e61f4dab8d46ed38748791c5d2da9cd38d6b0
borovan <84897664+borovan@users.noreply.github.com>
2025-10-23 12:16:40 +0200

    forgot update view impl

--- crates/mimic_declare/src/imp/update_view.rs
+++ crates/mimic_declare/src/imp/update_view.rs
@@ -11,7 +11,18 @@ pub struct UpdateViewTrait {}
 ///
 
 impl Imp<Entity> for UpdateViewTrait {
-    fn strategy(_: &Entity) -> Option<TraitStrategy> {
-        None
+    fn strategy(node: &Entity) -> Option<TraitStrategy> {
+        let update_ident = &node.update_ident();
+
+        // tokens
+        let q = quote! {
+            type View = #update_ident;
+        };
+
+        let tokens = Implementor::new(node.def(), Trait::UpdateView)
+            .set_tokens(q)
+            .to_token_stream();
+
+        Some(TraitStrategy::from_impl(tokens))
     }
 }
